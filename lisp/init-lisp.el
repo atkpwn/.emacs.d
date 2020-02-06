68976bd3dde47382729e96c47418d80d79baa1c8
Attakorn Putwattana <atkpwn@gmail.com>
2020-02-06 21:46:42 +0100

    move paren package from init-lisp to init-emacs

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -10,16 +10,4 @@
      (prettify-symbols-mode 1)
      (eldoc-mode 1))))
 
-(use-package paren
-  :custom
-  (show-paren-priority -1)
-  :config
-  (add-hook 'after-save-hook 'check-parens nil t)
-  (setq show-paren-delay 0)
-  (set-face-background 'show-paren-match (face-background 'default))
-  (set-face-foreground 'show-paren-match "#afa")
-  (set-face-attribute  'show-paren-match nil :weight 'extra-bold)
-  (set-face-background 'show-paren-mismatch "#a33")
-  (set-face-attribute  'show-paren-mismatch nil :weight 'extra-bold))
-
 (provide 'init-lisp)
