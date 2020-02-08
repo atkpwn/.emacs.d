2f97f09433c099aed981e88ee44000ec8a6e74e8
Attakorn Putwattana <atkpwn@gmail.com>
2020-02-09 00:38:46 +0100

    add and clean key binding

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -4,6 +4,14 @@
   (defvar org--prettify-symbols-alist
     '(("#+BEGIN_SRC" . ?↧)
       ("#+END_SRC"   . ?↥)))
+  :bind
+  (("C-c l" . org-store-link)
+   ("C-c a" . org-agenda)
+   :map org-src-mode-map
+   ("C-x C-s" . (lambda ()
+                  (interactive)
+                  (org-edit-src-exit)
+                  (save-buffer))))
   :hook
   (org-mode . (lambda ()
                 (setq-local prettify-symbols-alist org--prettify-symbols-alist)
@@ -13,12 +21,6 @@
   (org-confirm-babel-evaluate nil)
   :config
   (setq org-hide-emphasis-markers t)
-  (eval-after-load 'org-src
-    '(define-key org-src-mode-map
-       (kbd "C-x C-s") '(lambda ()
-                          (interactive)
-                          (org-edit-src-save)
-                          (save-buffer))))
   (org-babel-do-load-languages
    'org-babel-load-languages
    '((emacs-lisp . t)
